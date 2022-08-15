bd50ab7fbf083486cb69d41d450c3f7b34e4a483
GabrielArabe <65679811+GabrielArabe@users.noreply.github.com>
2022-08-14 22:22:20 -0300

    Created send and receive projects
    Created send and receive projects

---
+++ Send/Send/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Send // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "Hello",
+                                       durable: false,
+                                       exclusive: false,
+                                       autoDelete: false,
+                                       arguments: null);
+
+                
+                
+                for (int i = 0; i < 20; i++)
+                {
+                    var message = $"Message number {i}";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                 routingKey: "hello",
+                                 basicProperties: null,
+                                 body: body);
+
+                    Console.WriteLine("[x] Sent {0}", message);
+                }          
+
+           
+            }
+
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
